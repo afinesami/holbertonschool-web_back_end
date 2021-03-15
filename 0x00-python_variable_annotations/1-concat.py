b2ac222009f1c183463c7a4ea1ccecafd238feb5
lafine sami <samibenomar84@gmail.com>
2021-03-15 21:18:45 +0100

    annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""  Basic annotations - concat  """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ function concat """
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""  Basic annotations -  floor   """
+
+
+def floor(n: float) -> int:
+    """ function floor """
+    return int(n)
